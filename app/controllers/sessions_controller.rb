790c681374b2aff2cafd6f8f671e9c77f0847c95
Tanja Stroble <plusdeltah@gmail.com>
2017-10-16 15:53:56 -0700

    allows only log in with github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,33 +2,45 @@ class SessionsController < ApplicationController
   def login_form
   end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
-    redirect_to root_path
-  end
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     flash[:result_text] = "User not valid. Please log in or sign up with Github."
+  #     redirect_to login_path
+  #     return
+  #   end
+  #   redirect_to root_path
+  # end
 
   def logout
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
+  end
+
+  def create
+    @auth_hash = request.env['omniauth.auth']
+
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "#{@user.username}, logged in !"
+    else
+      @user = User.new uid: @auth_hash['uid'], provider: @auth_hash['provider'], username: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
+      if
+        @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.username}"
+      else
+        flash[:error] = "Unable to save user!"
+      end
+    end
     redirect_to root_path
   end
 end
